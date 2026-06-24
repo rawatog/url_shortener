7f34371e84275b5c156129c27542813219148e84
Loki King <loki@example.com>
2026-06-24 20:41:34 +0000

    updated for k8s

--- app/db.py
+++ app/db.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Use SQLite instead of PostgreSQL
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./urls.db")
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+)
 
-# connect_args={"check_same_thread": False} is required for SQLite
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
